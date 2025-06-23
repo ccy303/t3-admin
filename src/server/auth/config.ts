1b93a988db385a43e7e72452053e02c62cc8f6a5
ccy <303752792@qq.com>
2025-06-23 18:15:27 +0800

    init

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -11,15 +11,15 @@ import { db } from "~/server/db";
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  */
 declare module "next-auth" {
-  interface User {
-    id?: string | undefined; // Use string if you convert bigint to string
-    user_name: string | null;
-    // Add other fields as necessary
-  }
+    interface User {
+        id?: string | undefined; // Use string if you convert bigint to string
+        user_name: string | null;
+        // Add other fields as necessary
+    }
 
-  interface Session {
-    user: User & DefaultSession["user"];
-  }
+    interface Session {
+        user: User & DefaultSession["user"];
+    }
 }
 
 /**
@@ -28,69 +28,68 @@ declare module "next-auth" {
  */
 
 export const authConfig = {
-  providers: [
-    // DiscordProvider,
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: {},
-        password: {},
-      },
-      async authorize(credentials: any, req) {
-        const user = await db.sysUser.findFirst({
-          where: {
-            user_name: credentials?.username,
-            password: credentials?.password,
-          },
-        });
+    providers: [
+        // DiscordProvider,
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: {},
+                password: {},
+            },
+            async authorize(credentials: any, req) {
+                const user = await db.sysUser.findFirst({
+                    where: {
+                        user_name: credentials?.username,
+                        password: credentials?.password,
+                    },
+                });
 
-        if (user) {
-          // Convert id (and other bigint fields if needed) to string
-          return {
-            ...user,
-            id: user.id.toString(), // Ensure id is a string
-          };
-        }
+                if (user) {
+                    const { password, ...other } = user;
+                    // Convert id (and other bigint fields if needed) to string
+                    return {
+                        ...other,
+                        id: user.id.toString(), // Ensure id is a string
+                    };
+                }
 
-        return null;
-      },
-    }),
+                return null;
+            },
+        }),
 
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
-  ],
-  // adapter: PrismaAdapter(db),
-  callbacks: {
-    authorized: async ({ auth }) => {
-      return !!auth;
-      // You can add custom logic here to check if the user is authorized
-      // For example, you can check if the user has a specific role or permission
-      // For now, we will just return true to allow all authenticated users
+        /**
+         * ...add more providers here.
+         *
+         * Most other providers require a bit more work than the Discord provider. For example, the
+         * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
+         * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
+         *
+         * @see https://next-auth.js.org/providers/github
+         */
+    ],
+    // adapter: PrismaAdapter(db),
+    callbacks: {
+        authorized: async ({ auth }) => {
+            return !!auth;
+            // You can add custom logic here to check if the user is authorized
+            // For example, you can check if the user has a specific role or permission
+            // For now, we will just return true to allow all authenticated users
+        },
+        // session: async ({ session, token }) => {
+        //   // console.log("session", session);
+        //   // console.log("session.user", session.user);
+        //   // console.log("token", token);
+        // },
+        jwt: async ({ token, user }) => {
+            token.user = user;
+            return token;
+        },
     },
-    // session: async ({ session, token }) => {
-    //   // console.log("session", session);
-    //   // console.log("session.user", session.user);
-    //   // console.log("token", token);
-    // },
-    jwt: async ({ user }) => {
-      console.log("user in jwt callback", user);
-      // if (account) {
-      //   token.accessToken = account.access_token;
-      // }
+    pages: {
+        signIn: "/auth/signin",
+        // signOut: "/auth/signout",
+        // error: "/auth/error",
+        // verifyRequest: "/auth/verify-request",
+        // newUser: null, // Will disable the new account creation screen
     },
-  },
-  pages: {
-    signIn: "/auth/signin",
-    // signOut: "/auth/signout",
-    // error: "/auth/error",
-    // verifyRequest: "/auth/verify-request",
-    // newUser: null, // Will disable the new account creation screen
-  },
 } satisfies NextAuthConfig;
